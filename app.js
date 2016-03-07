6830d1541d0f5f3f877afca27f37f5ac12d7bdb9
unknown <bdcon734@MCOVCLT164939.ad.mvwcorp.com>
2016-03-07 16:01:33 -0500

    second commit

--- app.js
+++ app.js
@@ -1,29 +1,24 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//application root
+app.use('/', function(req, res){
+  res.render('index');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,5 +51,9 @@ app.use(function(err, req, res, next) {
   });
 });
 
+app.set("port", process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set("ip", process.env.OPENSHIFT_NODEJS_IP || 'localhost');
 
-module.exports = app;
+app.listen(app.get('port'), app.get('ip'), function(){
+  console.log("app is running at IP: " + app.get('ip') + " on port: " + app.get('port'));
+})
